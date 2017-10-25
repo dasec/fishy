d3d28554fbc0172aba87d28f8184bd362e80d56d
importantchoice <22687029+importantchoice@users.noreply.github.com>
2017-10-25 18:29:28 +0200

    FAT: fix:changed long filename encoding to UCS-2/UTF16

--- fishy/fat/fat.py
+++ fishy/fat/fat.py
@@ -220,7 +220,7 @@ class FAT:
                                 break
                         # append our lfn part to the global lfn, that will
                         # later used as the filename
-                        lfn = retlfn.decode('utf-8') + lfn
+                        lfn = retlfn.decode('utf-16') + lfn
                     else:
                         retlfn = lfn
                         lfn = ''
