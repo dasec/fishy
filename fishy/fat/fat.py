6f4e74ef7360c11521de8df8833069baa0a58e26
importantchoice <22687029+importantchoice@users.noreply.github.com>
2017-10-29 01:21:10 +0200

    FAT: added FAT12 support for writing FAT entries

--- fishy/fat/fat.py
+++ fishy/fat/fat.py
@@ -31,8 +31,7 @@ from io import BytesIO, BufferedReader
 FAT12PreDataRegion = Struct(
         "bootsector" / Embedded(FAT12_16Bootsector),
         Padding((this.reserved_sector_count - 1) * this.sector_size),
-        # FATs. Due to 12 bit addresses in FAT12 we will parse the
-        # actual structure later in __init__ method
+        # FATs.
         "fats" / Array(this.fat_count, Bytes(this.sectors_per_fat * this.sector_size)),
         # RootDir Table
         "rootdir" / Bytes(this.rootdir_entry_count * DirEntry.sizeof())
@@ -64,6 +63,7 @@ class FAT:
         """
         self.stream = stream
         self.offset = stream.tell()
+        self.predataregion_definition = predataregion
         self.pre = predataregion.parse_stream(stream)
         self.start_dataregion = stream.tell()
 
@@ -91,19 +91,22 @@ class FAT:
         # get start position of FAT0
         fat0_start = self.offset + 512 + (self.pre.sector_size - 512) + \
             (self.pre.reserved_sector_count - 1) * self.pre.sector_size
-        fat1_start = fat0_start + self.pre.sectors_per_fat * self.pre.sector_size
+        fat1_start = fat0_start + self.pre.sectors_per_fat \
+            * self.pre.sector_size
         # update first fat on disk
-        self.stream.seek(fat0_start + cluster_id * self._fat_entry.sizeof())
+        self.stream.seek(fat0_start + cluster_id
+                         * self._fat_entry.sizeof())
         self.stream.write(self._fat_entry.build(value))
         # update second fat on disk if it exists
         if self.pre.fat_count > 1:
-            self.stream.seek(fat1_start + cluster_id * self._fat_entry.sizeof())
+            self.stream.seek(fat1_start + cluster_id
+                             * self._fat_entry.sizeof())
             self.stream.write(self._fat_entry.build(value))
         # flush changes to disk
         self.stream.flush()
         # re-read fats into memory
-        fat_definition = Array(self.pre.fat_count, 
-                               Bytes(self.pre.sectors_per_fat * 
+        fat_definition = Array(self.pre.fat_count,
+                               Bytes(self.pre.sectors_per_fat *
                                      self.pre.sector_size))
         self.stream.seek(fat0_start)
         self.pre.fats = fat_definition.parse_stream(self.stream)
@@ -294,10 +297,56 @@ class FAT12(FAT):
             hexvalue = sl.hex()
             value = int(hexvalue[3] + hexvalue[0:2], 16)
         else:
-            # if cluster_number is odd, we need to wipe the first nibble
+            # if cluster_number is odd, we need to wipe the second nibble
             hexvalue = sl.hex()
             value = int(hexvalue[2:4] + hexvalue[0], 16)
         return self._fat_entry.parse(value.to_bytes(2, 'little'))
+
+    def write_fat_entry(self, cluster_id, value):
+        # make sure user does not input invalid values as next cluster
+        if type(value) == int:
+            assert value <= 4086, "next_cluster value must be <= 4086. For " \
+                                  + "last cluster use 'last_cluster'. For " \
+                                  + "bad_cluster use 'bad_cluster'"
+            assert value >= 2, "next_cluster value must be >= 2. For " \
+                               + "free_cluster use 'free_cluster'"
+        # get start position of FAT0
+        fat0_start = self.offset + 512 + (self.pre.sector_size - 512) + \
+            (self.pre.reserved_sector_count - 1) * self.pre.sector_size
+        fat1_start = fat0_start + self.pre.sectors_per_fat \
+            * self.pre.sector_size
+        # read current entry
+        byte = cluster_id + int(cluster_id/2)
+        self.stream.seek(fat0_start + byte)
+        current_entry = self.stream.read(2).hex()
+        new_entry_hex = self._fat_entry.build(value).hex()
+        # calculate new entry as next entry overlaps with current bytes
+        if cluster_id % 2 == 0:
+            # if cluster_number is even, we need to keep the third nibble
+            new_entry = new_entry_hex[0:2] + current_entry[2] \
+                + new_entry_hex[3]
+        else:
+            # if cluster_number is odd, we need to keep the second nibble
+            new_entry = new_entry_hex[1] + current_entry[1] + \
+                    new_entry_hex[3] + new_entry_hex[0]
+        # convert hex to bytes
+        new_entry = bytes.fromhex(new_entry)
+        print(new_entry)
+        # write new value to first fat on disk
+        self.stream.seek(fat0_start + byte)
+        self.stream.write(new_entry)
+        # write new value to second fat on disk if it exists
+        if self.pre.fat_count > 1:
+            self.stream.seek(fat1_start + byte)
+            self.stream.write(new_entry)
+        # flush changes to disk
+        self.stream.flush()
+        # re-read fats into memory
+        fat_definition = Array(self.pre.fat_count,
+                               Bytes(self.pre.sectors_per_fat *
+                                     self.pre.sector_size))
+        self.stream.seek(fat0_start)
+        self.pre.fats = fat_definition.parse_stream(self.stream)
 
     def _root_to_stream(self, stream):
         """
