cf0a60ca9e0f117662d57a2a6fa667268012de42
importantchoice <22687029+importantchoice@users.noreply.github.com>
2017-11-04 16:20:42 +0100

    FAT: FileSlack: Created unit tests

---
+++ fishy/fat/file_slack_test.py
@@ -0,0 +1,110 @@
+import unittest
+from .file_slack import FileSlack
+import os
+import io
+import subprocess
+
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
+utilsdir = os.path.join(this_dir, os.pardir, os.pardir, 'utils')
+
+class TestFatFileSlack(unittest.TestCase):
+    image_paths = [ 
+                    os.path.join(utilsdir, 'testfs-fat12.dd'),
+                    os.path.join(utilsdir, 'testfs-fat16.dd'),
+                    os.path.join(utilsdir, 'testfs-fat32.dd'),
+                  ]
+
+    @classmethod
+    def setUpClass(cls):
+        # regenerate test filesystems
+        cmd = os.path.join(utilsdir, "create_testfs.sh") + " " + utilsdir
+        subprocess.call(cmd, shell=True)
+
+    def test_find_file(self):
+        for img_path in TestFatFileSlack.image_paths:
+            with open(img_path, 'rb') as f:
+                # create FileSlack object
+                fs = FileSlack(f)
+                result = fs._find_file("long_file.txt")
+                self.assertEqual(result.name, b'LONG_F~1')
+                self.assertEqual(result.extension, b'TXT')
+                self.assertFalse(result.attributes.unused)
+                self.assertFalse(result.attributes.device)
+                self.assertTrue(result.attributes.archive)
+                self.assertFalse(result.attributes.subDirectory)
+                self.assertFalse(result.attributes.volumeLabel)
+                self.assertFalse(result.attributes.system)
+                self.assertFalse(result.attributes.hidden)
+                self.assertFalse(result.attributes.readonly)
+                self.assertEqual(result.fileSize, 8001)
+
+    def test_write_file(self):
+        for img_path in TestFatFileSlack.image_paths:
+            with open(img_path, 'rb+') as f:
+                # create FileSlack object
+                fs = FileSlack(f)
+                with io.BytesIO() as mem:
+                    teststring = "This is a simple write test."
+                    mem.write(teststring.encode('utf-8'))
+                    mem.seek(0)
+                    with io.BufferedReader(mem) as reader:
+                        result = fs.write(reader, ['another'])
+                        self.assertEqual(result.clusters, [(3, 512, 28)] )
+
+    def test_write_file_in_subdir(self):
+        # only testing fat12 as resulting cluster_id of different fat
+        # versions differs
+
+        img_path = TestFatFileSlack.image_paths[0]
+        with open(img_path, 'rb+') as f:
+            # create FileSlack object
+            fs = FileSlack(f)
+            with io.BytesIO() as mem:
+                teststring = "This is a simple write test."
+                mem.write(teststring.encode('utf-8'))
+                mem.seek(0)
+                with io.BufferedReader(mem) as reader:
+                    result = fs.write(reader,
+                                    ['onedirectory/afileinadirectory.txt'])
+                    self.assertEqual(result.clusters, [(9, 512, 28)] )
+
+    def test_read_slack(self):
+        for img_path in TestFatFileSlack.image_paths:
+            with open(img_path, 'rb+') as f:
+                # create FileSlack object
+                fs = FileSlack(f)
+                teststring = "This is a simple write test."
+                # write content that we want to read
+                with io.BytesIO() as mem:
+                    mem.write(teststring.encode('utf-8'))
+                    mem.seek(0)
+                    with io.BufferedReader(mem) as reader:
+                        write_res = fs.write(reader, ['another'])
+                with io.BytesIO() as mem:
+                    fs.read(mem, write_res)
+                    mem.seek(0)
+                    result = mem.read()
+                    self.assertEqual(result.decode('utf-8'), teststring)
+
+    def test_clear_slack(self):
+        for img_path in TestFatFileSlack.image_paths:
+            with open(img_path, 'rb+') as f:
+                # create FileSlack object
+                fs = FileSlack(f)
+                teststring = "This is a simple write test."
+                # write content that we want to read
+                with io.BytesIO() as mem:
+                    mem.write(teststring.encode('utf-8'))
+                    mem.seek(0)
+                    with io.BufferedReader(mem) as reader:
+                        write_res = fs.write(reader, ['another'])
+                    fs.clear(write_res)
+                with io.BytesIO() as mem:
+                    fs.read(mem, write_res)
+                    mem.seek(0)
+                    result = mem.read()
+                    expected = len(teststring.encode('utf-8')) * b'\x00'
+                    self.assertEqual(result, expected)
+if __name__ == '__main__':
+    unittest.main()
--- utils/create_testfs.sh
+++ utils/create_testfs.sh
@@ -9,14 +9,21 @@
 # * a directory called 'fs-files' including the filestructure
 #   that will be copied into the created filesystem images
 
+workingdir="$1"
 filestructure="fs-files"
+echo "WorkingDir: $workingdir"
+if [ "$workingdir" == "" ]; then
+	workingdir="$(pwd)"
+fi
+
+filestructure="$workingdir/$filestructure"
 
 # check if required directories exist
 if [ ! -d "$filestructure" ]; then
 	echo "directory '$filestructure' is missing"
 	exit 1
 fi
-if [ ! -d "mount-fs" ]; then
+if [ ! -d "$workingdir/mount-fs" ]; then
 	echo "directory 'mount-fs' is missing"
 	exit 1
 fi
@@ -25,27 +32,27 @@ function copy_files {
 	# this function copies the content of $filestructure
 	# into a given filesystem image
 	# usage copy_files [FILESYSTEM_IMAGE]
-	sudo mount "$1" mount-fs
-	sudo cp -r "$filestructure"/* mount-fs
-	sudo umount mount-fs
+	sudo mount "$1" "$workingdir"/mount-fs
+	sudo cp -r "$filestructure"/* "$workingdir"/mount-fs
+	sudo umount "$workingdir"/mount-fs
 }
 
 # Create a 1MB FAT12 image
-dd if=/dev/zero of=testfs-fat12.dd bs=512 count=2000
-mkfs.vfat testfs-fat12.dd
-copy_files testfs-fat12.dd
+dd if=/dev/zero of="$workingdir/testfs-fat12.dd" bs=512 count=2000
+mkfs.vfat "$workingdir/testfs-fat12.dd"
+copy_files "$workingdir/testfs-fat12.dd"
 
 # Create a 512MB FAT16 image
-dd if=/dev/zero of=testfs-fat16.dd bs=512 count=1000000
-mkfs.vfat testfs-fat16.dd
-copy_files testfs-fat16.dd
+dd if=/dev/zero of="$workingdir/testfs-fat16.dd" bs=512 count=1000000
+mkfs.vfat "$workingdir/testfs-fat16.dd"
+copy_files "$workingdir/testfs-fat16.dd"
 
 # Create a 2GB FAT32 image
-dd if=/dev/zero of=testfs-fat32.dd bs=512 count=4000500
-mkfs.vfat testfs-fat32.dd
-copy_files testfs-fat32.dd
+dd if=/dev/zero of="$workingdir"/testfs-fat32.dd bs=512 count=4000500
+mkfs.vfat "$workingdir/testfs-fat32.dd"
+copy_files "$workingdir/testfs-fat32.dd"
 
 # Create a 10MB NTFS image
-dd if=/dev/zero of=testfs-ntfs.dd bs=512 count=20000
-mkfs.ntfs -F testfs-ntfs.dd
-copy_files testfs-ntfs.dd
+dd if=/dev/zero of="$workingdir/testfs-ntfs.dd" bs=512 count=20000
+mkfs.ntfs -F "$workingdir/testfs-ntfs.dd"
+copy_files "$workingdir/testfs-ntfs.dd"
