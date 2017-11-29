a0a3d9d3d4ba01f7384e8b2e33aaaef6b19d3378
stnonguy <ngoc.m.c.nguyen@stud.h-da.de>
2017-11-29 11:57:15 +0100

    some code fixes

--- fishy/ntfs/ntfs_boot.py
+++ fishy/ntfs/ntfs_boot.py
@@ -1,22 +1,24 @@
+"""
+interface for ntfs boot sector
+"""
 import os
 import hashlib
 
-class NTFSMeta: 
-
+class NTFSMeta:
+	""" class to analyze and scan boot sector of ntfs systems.
+	"""
 	def __init__(self, file):
 		""" init: open ntfs image
 		file: saves file name
-		""" 
+		"""
 		self.file = file
-
-		if os.path.isfile(self.file): 
+		if os.path.isfile(self.file):
 			print("ok")
 			# test.is_ntfs() TODO is this needed?
 		else:
-			print ("file does not exist.")
-			return 0
+			print("file does not exist.")
 
-	def is_altered(self): 
+	def is_altered(self):
 		""" is_altered:
 		compare boot sector with the backup boot sector (via hash values)
 		true: the sector was altered
@@ -30,7 +32,6 @@ class NTFSMeta:
 			# create hash value for boot sector
 			hash_boot = hashlib.sha256()
 			hash_boot.update(data)
-
 			# determine file size
 			statinfo = os.stat(self.file)
 			# skip to last partition (backup boot sector)
@@ -39,44 +40,42 @@ class NTFSMeta:
 			# create hash value for backup boot sector
 			hash_back = hashlib.sha256()
 			hash_back.update(backup)
-
 			# compare hash values
 			if hash_boot.hexdigest() == hash_back.hexdigest():
 				# nothing to do here
-				print ("boot sector was not compromised.")
+				print("boot sector was not compromised.")
 				return False
 			else:
 				# compromised, further action required
-				print ("boot sector was compromised.")
+				print("boot sector was compromised.")
 				return True
 		except IOError:
 			return 0
 		finally:
 			file.close()
 
-	def scanBoot(self):
-		""" scanBoot: scans the boot sector for hidden data
+	def scan_boot_sector(self):
+		""" scan_boot_sector: scans the boot sector for hidden data
 		"""
-		with open(self.file, "rb") as f:
+		with open(self.file, "rb") as file:
 			offset = 512
-			# 	f.seek(0) jump to start of file needed?
-			pos = f.tell()
+			# 	file.seek(0) jump to start of file needed?
+			pos = file.tell()
 			current_pos = 0
-			last = os.stat(filename).st_size - offset
+			last = os.stat(self.file).st_size - offset
 			while current_pos < offset:
 				# switch cursor between current position at boot & backup sector
-				f.seek(pos + current_pos)
-				c = f.read(1)
-				f.seek(last + current_pos)
-				d = f.read(1)
-				if not c:
-				print ("End of file")
-				break
-				if not c == d:
-				# do something 
-				print(c)
+				file.seek(pos + current_pos)
+				boot_c = file.read(1)
+				file.seek(last + current_pos)
+				back_c = file.read(1)
+				if not boot_c:
+					print("End of file")
+					break
+				if boot_c != back_c:
+					# do something
+					print(boot_c)
 				current_pos = current_pos + 1
-			f.seek(pos)
-			f.close()
-
+			file.seek(pos)
+			file.close()
 
