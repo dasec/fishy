0afd7d591e830b3b6e20aec6731972e3c3df42fd
celikd <7234110+celikd@users.noreply.github.com>
2017-12-09 18:05:13 +0100

    added gdt to base ext4 class

--- fishy/ext4/EXT4.py
+++ fishy/ext4/EXT4.py
@@ -1,6 +1,8 @@
-from .superblock import Superblock
+from fishy.ext4.superblock import Superblock
+from fishy.ext4.ext4_gdt import GDT
 
 class EXT4:
 
     def __init__(self, image):
         self.superblock = Superblock(image)
+        self.gdt = GDT(image, self.superblock)
