c630962ca53bb780a3eb8f27cda2737302cfc6a8
celikd <7234110+celikd@users.noreply.github.com>
2017-12-11 03:57:02 +0100

    GDT class now contains the whole GDT as opposed to only a single group descriptor

--- fishy/ext4/ext4_gdt.py
+++ fishy/ext4/ext4_gdt.py
@@ -1,7 +1,9 @@
 from fishy.ext4.parser import Parser
+import math
 
 class GDT:
 
+    # represents a 32 bit group descriptor
     structure32 = {
         "block_bitmap_lo":          {"offset": 0x0, "size": 4},
         "inode_bitmap_lo":          {"offset": 0x4, "size": 4},
@@ -17,7 +19,7 @@ class GDT:
         "checksum":                 {"offset": 0x1E, "size": 2}
     }
 
-    # Contains fields only used if the 64bit feature is enabled and s_desc_size > 32.
+    # represents a 64 bit group descriptor
     structure64 = {
         "block_bitmap_lo":          {"offset": 0x0, "size": 4},
         "inode_bitmap_lo":          {"offset": 0x4, "size": 4},
@@ -31,7 +33,7 @@ class GDT:
         "inode_bitmap_csum_lo":     {"offset": 0x1A, "size": 2},
         "itable_unused_lo":         {"offset": 0x1C, "size": 2},
         "checksum":                 {"offset": 0x1E, "size": 2},
-        # These fields only exist if the 64bit feature is enabled and s_desc_size > 32.
+        # these fields only exist if the 64bit feature is enabled and s_desc_size > 32.
         "block_bitmap_hi":          {"offset": 0x20, "size": 4},
         "inode_bitmap_hi":          {"offset": 0x24, "size": 4},
         "inode_table_hi":           {"offset": 0x28, "size": 4},
@@ -45,17 +47,33 @@ class GDT:
         "reserved":                 {"offset": 0x3C, "size": 4}
     }
 
-    def __init__(self, filename, superblock):
+    def __init__(self, fs_stream, superblock, blocksize):
+        self.blocksize = blocksize
         if (int(superblock.data['feature_incompat'], 0) & 0x80) == 0x80:
             self.is_64bit = True
         else:
             self.is_64bit = False
-        self.data = self.parse_gdt(filename)
+        # contains a list of all group descriptors
+        self.data = self.parse_gdt(fs_stream, superblock)
 
-    def parse_gdt(self, filename):
-        if self.is_64bit:
-            d = Parser.parse(filename, 1024, 64, structure=self.structure64)
+    def parse_gdt(self, fs_stream, superblock):
+        total_block_count = superblock.data['total_block_count']
+        blocks_per_group = superblock.data['blocks_per_group']
+        total_block_group_count = int(math.ceil(total_block_count / blocks_per_group))
+        data = []
+        if self.blocksize == 1024:
+            offset = 2048
         else:
-            d = Parser.parse(filename, 1024, 32, structure = self.structure32)
+            offset = self.blocksize
 
-        return d
+        if self.is_64bit:
+            for i in range(0, total_block_group_count):
+                data.append(Parser.parse(fs_stream, offset, 64, structure=self.structure64))
+                offset += 64
+        else:
+            offset = 1024
+            for i in range(0, total_block_group_count):
+                data.append(Parser.parse(fs_stream, offset, 32, structure=self.structure32))
+                offset += 32
+
+        return data
