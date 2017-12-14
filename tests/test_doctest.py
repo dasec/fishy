af3d3044aec587f1d9b6b71749e8ef1e910b82c9
importantchoice <22687029+importantchoice@users.noreply.github.com>
2017-12-14 12:56:05 +0100

    added doctest xml output

--- tests/test_doctest.py
+++ tests/test_doctest.py
@@ -4,9 +4,16 @@ This test module runs doctests against selected modules
 
 import doctest
 import unittest
+import xmlrunner
 import fishy.metadata
 
 def load_tests(loader, tests, ignore):
     # add doctests for metadata module
     tests.addTests(doctest.DocTestSuite(fishy.metadata))
     return tests
+
+if __name__ == '__main__':
+    with open('doctests.xml', 'wb') as output:
+            unittest.main(
+                testRunner=xmlrunner.XMLTestRunner(output=output),
+                failfast=False, buffer=False, catchbreak=False)
